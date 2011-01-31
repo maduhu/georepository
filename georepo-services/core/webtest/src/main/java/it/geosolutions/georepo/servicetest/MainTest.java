25e3f28a71e96803bd4cc2d90cfb6154cc8b0f25
ETj <etj@geo-solutions.it>
2011-01-31 15:26:44 +0100

    MainTest: populate db with test data.

--- georepo-services/core/webtest/src/main/java/it/geosolutions/georepo/servicetest/MainTest.java
+++ georepo-services/core/webtest/src/main/java/it/geosolutions/georepo/servicetest/MainTest.java
@@ -20,12 +20,22 @@
 
 package it.geosolutions.georepo.servicetest;
 
+import it.geosolutions.georepo.core.model.GSInstance;
 import it.geosolutions.georepo.core.model.GSUser;
+import it.geosolutions.georepo.core.model.LayerAttribute;
+import it.geosolutions.georepo.core.model.LayerDetails;
+import it.geosolutions.georepo.core.model.Profile;
+import it.geosolutions.georepo.core.model.Rule;
+import it.geosolutions.georepo.core.model.enums.AccessType;
+import it.geosolutions.georepo.core.model.enums.GrantType;
 import it.geosolutions.georepo.services.InstanceAdminService;
 import it.geosolutions.georepo.services.ProfileAdminService;
 import it.geosolutions.georepo.services.RuleAdminService;
 import it.geosolutions.georepo.services.UserAdminService;
+import it.geosolutions.georepo.services.dto.ShortProfile;
+import it.geosolutions.georepo.services.dto.ShortRule;
 import it.geosolutions.georepo.services.dto.ShortUser;
+import it.geosolutions.georepo.services.exception.ResourceNotFoundFault;
 import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
@@ -46,25 +56,52 @@ public class MainTest implements InitializingBean {
     protected final static String MULTIPOLYGONWKT = "MULTIPOLYGON(((48.6894038 62.33877482, 48.7014874 62.33877482, 48.7014874 62.33968662, 48.6894038 62.33968662, 48.6894038 62.33877482)))";
 
     public void afterPropertiesSet() throws Exception {
-        LOGGER.info("===== Starting tests =====");
-
-        LOGGER.info("===== Removing old users =====");
-
-        for (ShortUser shortUser : userAdminService.getAll()) {
-            System.out.println("Removing user -> " + shortUser);
-            userAdminService.delete(shortUser.getId());
-        }
+        LOGGER.info("===== RESETTING DB DATA =====");
+        removeAll();
         
-//        LOGGER.info("===== Creating Users =====");
-//        Profile profile = new Profile();
-//        profile.setName("profile1");
-//
-//
-//        GSUser u1 = createUser("user1");
-//        userAdmin.insert(u1);
-//
-//        GSUser u2 = createUser("user2");
-//        userAdmin.insert(u2);
+        LOGGER.info("===== Creating Profiles =====");
+        ShortProfile shortProfile = new ShortProfile();
+        shortProfile.setName("profile1");
+        long pid1 = profileAdminService.insert(shortProfile);
+        Profile p1 = profileAdminService.get(pid1);
+
+        ShortProfile shortProfile2 = new ShortProfile();
+        shortProfile2.setName("profile2");
+        long pid2 = profileAdminService.insert(shortProfile2);
+        Profile p2 = profileAdminService.get(pid2);
+
+
+        LOGGER.info("===== Creating Users =====");
+        GSUser u1 = createUser("user1");
+        userAdminService.insert(u1);
+
+        GSUser u2 = createUser("user2");
+        userAdminService.insert(u2);
+
+        LOGGER.info("===== Creating Rules =====");
+
+        LayerDetails ld1 = new LayerDetails();
+        ld1.getAllowedStyles().add("style1");
+        ld1.getAllowedStyles().add("style2");
+        ld1.getAttributes().add(new LayerAttribute("attr1", AccessType.NONE));
+        ld1.getAttributes().add(new LayerAttribute("attr2", AccessType.READONLY));
+        ld1.getAttributes().add(new LayerAttribute("attr3", AccessType.READWRITE));
+
+        int pri = -1;
+        Rule rules[] = new Rule[100];
+
+
+
+        pri++; rules[pri] = new Rule(pri, null, null, null,   "WCS", null, null, null, GrantType.ALLOW);
+        pri++; rules[pri] = new Rule(pri, null, null, null,   "s1", "r2", "w2", "l2", GrantType.ALLOW);
+        pri++; rules[pri] = new Rule(pri, null, null, null,   "s3", "r3", "w3", "l3", GrantType.ALLOW);
+        pri++; rules[pri] = new Rule(pri, null, null, null,    null, null, null, null, GrantType.DENY);
+
+        for (Rule rule : rules) {
+            if(rule != null)
+                ruleAdminService.insert(rule);
+        }
+
 
         try {
             LOGGER.info("===== User List =====");
@@ -74,10 +111,9 @@ public class MainTest implements InitializingBean {
                 System.out.println("User -> " + loop);
             }
 
+
+
         } finally {
-//            watchAdmin.deleteWatch(nw.getId());
-//            watchAdmin.deleteWatch(dw.getId());
-//            aoiAdmin.deleteAoi(aoiId);
         }
     }
 
@@ -87,6 +123,69 @@ public class MainTest implements InitializingBean {
         GSUser user = new GSUser();
         user.setName(baseName);
         return user;
+    }
+
+    //==========================================================================
+
+    protected void removeAll() throws ResourceNotFoundFault {
+        LOGGER.info("***** removeAll()");
+        removeAllRules();
+        removeAllUsers();
+        removeAllProfiles();
+        removeAllInstances();
+    }
+
+    protected void removeAllRules() throws ResourceNotFoundFault {
+        List<ShortRule> list = ruleAdminService.getAll();
+        for (ShortRule item : list) {
+            LOGGER.info("Removing " + item);
+            boolean ret = ruleAdminService.delete(item.getId());
+            if(!ret)
+                throw new IllegalStateException("Rule not removed");
+        }
+
+        if( ruleAdminService.getCountAll() != 0)
+                throw new IllegalStateException("Rules have not been properly deleted");
+    }
+
+    protected void removeAllUsers() throws ResourceNotFoundFault {
+        List<ShortUser> list = userAdminService.getAll();
+        for (ShortUser item : list) {
+            LOGGER.info("Removing " + item);
+            boolean ret = userAdminService.delete(item.getId());
+            if(!ret)
+                throw new IllegalStateException("User not removed");
+        }
+
+        if( userAdminService.getCount(null) != 0)
+                throw new IllegalStateException("Users have not been properly deleted");
+    }
+
+    protected void removeAllProfiles() throws ResourceNotFoundFault {
+        List<ShortProfile> list = profileAdminService.getAll();
+        for (ShortProfile item : list) {
+            LOGGER.info("Removing " + item);
+            boolean ret = profileAdminService.delete(item.getId());
+            if(!ret)
+                throw new IllegalStateException("Profile not removed");
+        }
+
+        if( profileAdminService.getCount(null) != 0)
+                throw new IllegalStateException("Profiles have not been properly deleted");
+    }
+
+    protected void removeAllInstances() throws ResourceNotFoundFault {
+        List<GSInstance> list = instanceAdminService.getAll();
+        for (GSInstance item : list) {
+            LOGGER.info("Removing " + item);
+            boolean ret = instanceAdminService.delete(item.getId());
+            if(!ret)
+                throw new IllegalStateException("GSInstance not removed");
+
+        }
+
+        if( instanceAdminService.getCount(null) != 0)
+                throw new IllegalStateException("Instances have not been properly deleted");
     }
 
     //==========================================================================
