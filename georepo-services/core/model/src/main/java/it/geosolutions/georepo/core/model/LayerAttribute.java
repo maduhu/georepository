dc5d2af8b1242d6b99e179abbb797af12c532bcd
ETj <etj@geo-solutions.it>
2011-07-04 21:23:38 +0200

    #11 Delete and insert attributes when selection Added missing equals() and hashCode() methods.

--- georepo-services/core/model/src/main/java/it/geosolutions/georepo/core/model/LayerAttribute.java
+++ georepo-services/core/model/src/main/java/it/geosolutions/georepo/core/model/LayerAttribute.java
@@ -99,6 +99,38 @@ public class LayerAttribute implements Serializable {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LayerAttribute other = (LayerAttribute) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        if ((this.datatype == null) ? (other.datatype != null) : !this.datatype.equals(other.datatype)) {
+            return false;
+        }
+        if (this.access != other.access) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + (this.name != null ? this.name.hashCode() : 0);
+        hash = 29 * hash + (this.datatype != null ? this.datatype.hashCode() : 0);
+        hash = 29 * hash + (this.access != null ? this.access.hashCode() : 0);
+        return hash;
+    }
+
+
+
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(getClass().getSimpleName())
                 .append("[name:").append(name)
